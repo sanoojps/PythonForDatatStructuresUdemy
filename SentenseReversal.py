28154e705a8342db0068aa7e77c4a7e621bca987
sanoojps <sanojneo@yahoo.co.in>
2018-07-29 07:06:19 +0530

    String
    String Reversal
    String Compression

--- SentenseReversal.py
+++ SentenseReversal.py
@@ -43,14 +43,12 @@ def reverse_a_string_of_words(string_of_words: str):
     
     
     #remove trailing and leading white space
-    string_of_words = \
-    string_of_words.lstrip().rstrip()
+    #string_of_words = \
+    #string_of_words.lstrip().rstrip()
     
     
     #enumerate through word
-    
     words = []
-    
     index = 0
     
     array_of_words = \
@@ -66,18 +64,21 @@ def reverse_a_string_of_words(string_of_words: str):
                 index
                 )
         
-        words.append(word_order)
+        if len(word_order.word) != 0:
+            words.append(word_order)
         
         index+=1
         
-        
+    print("_words:  %s" %(words))
+    
+    # reversing
     words_reversed = []
     
     index = len(words) - 1 
     
     for word in words:
         
-        if words[index] != '':
+        if len(words[index].word) != 0:
             words_reversed.append(words[index])
         
         index-=1
@@ -86,5 +87,90 @@ def reverse_a_string_of_words(string_of_words: str):
     
 #reverse_a_string_of_words("    the very best    ")
 
-atr = "Hi John,    are you ready to go?"    
+atr = "    Hi John,    are you ready to go?"    
 reverse_a_string_of_words(atr)
+
+
+
+def reverse_word_preferred(s):
+    
+    words = []
+    length = len(s)
+    spaces = [' ']
+
+    i = 0
+    
+    while i < length:
+        
+        # check if char is space
+        if s[i] not in spaces:
+          
+            # start counting the word
+            word_begin = i
+            
+            # find the end of the word
+            # lopp till another space is found
+            while i < length and s[i] not in spaces:
+                
+                #kep looping
+                i += 1
+                
+            # space found
+            # word ended
+            words.append(s[word_begin:i])
+            
+        # keep looping    
+        i += 1
+        
+    return " ".join(reversed(words))
+
+print("reverse_word_preferred")
+print(reverse_word_preferred(atr))
+                
+def reverse_word_preferred_for_loop(string_of_words: str):
+    
+    words = []
+    length = len(string_of_words)
+    spaces = [' ','']
+
+    # index to detect end of string
+    i = 0                
+    
+    # each word in the string
+    word_substring = ""
+    
+    print("length %s" %(length))
+    
+    for char in string_of_words:
+        
+        i += 1
+        
+        # check if it is space
+        if char not in spaces:
+            # not space
+
+            # add to word array 
+            word_substring += char
+        
+            print(word_substring)
+            print(i)
+            
+            # last word
+            if i == length:
+                 words.append(word_substring)
+        
+        # go till next space
+        else:
+            
+            # add word to dict
+            if len(word_substring) > 0:
+                words.append(word_substring)
+            
+            # reset substring
+            word_substring = ""
+            
+    print(words)
+    print(" ".join(reversed(words)))
+         
+print("reverse_word_preferred_for_loop")
+reverse_word_preferred_for_loop(atr)
---
+++ StringCompression.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 29 05:18:11 2018
+
+@author: sanooj
+"""
+
+"""
+
+input AAAABBBBCCCCCCDDEEEEE
+output A4B4C5D2E4
+
+input AAB
+output A2B1
+
+case sensitive
+in: AAAaaa
+out: A3a3
+
+"""
+
+# O(2n)
+def compress_string(string: str):
+    
+    # remove all spaces
+    
+    char_counts_dict = {}
+    
+    for char in string:
+        
+        count = char_counts_dict.get(char)
+        
+        # element not present
+        if count == None:
+            
+            char_counts_dict[char] = 1
+            
+        else:
+            
+            char_counts_dict[char] = count + 1
+            
+    print(char_counts_dict)
+    
+    compressed_string = ""
+    
+    for key in char_counts_dict:
+        
+        compressed_string += (key + str(char_counts_dict[key]))
+        
+    print(compressed_string)
+        
+    
+compress_string("AAB")
+compress_string("AAAABBBBCCCCCCDDEEEEE")
+compress_string("AAAaaa")
+        
+
+"""
+Does not use a data structure
+
+This solution compresses without checking. 
+known as the RunLength Compression algorithm
+
+O(n)
+
+"""
+def compress_string_preferred_solution(string: str):
+    
+    compressed_string = ""
+    
+    length = len(string)
+    
+    # edge case
+    
+    # length 0
+    if length == 0:
+        return ""
+    
+    # length 1
+    if length == 1:
+        return string+"1"
+    
+    count = 1
+    
+    index = 0
+    
+    # loop through all chars
+    for char in string:
+        
+        # skip first index
+        if index == 0:
+            index+=1
+            continue
+        
+        # check for repetition
+        if string[index] == string[index -1]:
+            
+            #repetition
+            count+=1
+            
+        # no repetition
+        # new char
+        else:
+            
+            # update string
+            compressed_string = \
+            compressed_string \
+            + string[index - 1] \
+            + str(count)
+            
+            # reset count
+            count = 1
+    
+        print(compressed_string)
+        
+        # increment index
+        index+=1
+    
+    # add final char
+    compressed_string = \
+            compressed_string \
+            + string[index - 1] \
+            + str(count) 
+            
+    print(compressed_string)
+    print(index)
+        
+print("compress_string_preferred_solution")
+compress_string_preferred_solution("AAB")
+    
+    
+    
