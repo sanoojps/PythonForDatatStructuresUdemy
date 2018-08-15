c2af36df2b7b45d1ccf9ecb5f1618b39bea69c67
sanoojps <sanojneo@yahoo.co.in>
2018-08-16 00:58:50 +0530

    Tree
    Tree

---
+++ Trees.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 13:35:23 2018
+
+@author: sanooj
+"""
+
+"""
+
+root
+
+leaves
+
+node
+--- key
+--- payload
+
+edge
+-----
+
+# connects two nodes
+
+path
+-----
+
+# ordered list of nodes that are connected by edges
+
+
+children
+---------
+
+set of nodes "c" -> that have incoming edges from the same node
+ 
+
+parent
+---------
+
+a node is the parent of all the nodes it
+connects to with outgoing edges
+
+Sibling
+-------
+
+children of the same parent are said to be siblings
+
+Subtree
+-------
+
+set of nodes and edges comprised of a parent
+and all the descendants of that parent
+
+Leaf node
+---------
+
+node with no children
+
+Level
+-----
+
+n ->
+
+number of edges on the path from the root node to "n"
+
+
+Height
+-------
+
+maximum level of any node in the tree
+
+
+"""
+
+  
